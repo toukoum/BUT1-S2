d739c31d239204c7e4b6d2b5037f2b34e0c972b8
Raphael Giraud <raphael.giraud@etu.univ-grenoble-alpes.fr>
2023-03-27 13:49:14 +0200

    test miroring

--- src/TP5/tableDOperation/TestTableDOperation.java
+++ src/TP5/tableDOperation/TestTableDOperation.java
@@ -83,7 +83,7 @@ public class TestTableDOperation {
         // Affichage du résultat
         System.out.println("\n--- Résultat ---");
         System.out.println("Vous avez " + tableDOperation.getNombreReponseJuste() + " réponses justes sur " + tableDOperation.getNombreOperation() + " opérations.");
-
+        // rodrimax : pourquoi il aussi bo ?
 
     }
 
